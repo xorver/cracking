92e920ec9fcbd32a54b394cc2f7fb26b2f3e495e
Tomasz Lichon <xorver@gmail.com>
2016-07-30 23:31:07 +0200

    Add ex616 solution.

---
+++ src/ex616/Indices.java
@@ -0,0 +1,14 @@
+package ex616;
+
+public class Indices {
+    public int begin = -1;
+    public int end = -1;
+
+    @Override
+    public String toString() {
+        return "Indices{" +
+                "begin=" + begin +
+                ", end=" + end +
+                '}';
+    }
+}
