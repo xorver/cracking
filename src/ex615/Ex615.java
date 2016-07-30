5e174a0e679712b7ed4e2d71d023a5c7aa9dc4d0
Tomasz Lichon <xorver@gmail.com>
2016-07-30 23:16:16 +0200

    Add ex615 solution.

---
+++ src/ex615/Ex615.java
@@ -0,0 +1,29 @@
+package ex615;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex615 {
+
+    public static Score getScore(String actual, String guess) {
+        Map<Character, Integer> map = new HashMap<>();
+        Score score = new Score();
+        for(int i = 0; i < actual.length(); i++)
+            if(actual.charAt(i) == guess.charAt(i))
+                score.hit++;
+            else
+                map.put(actual.charAt(i), map.getOrDefault(actual.charAt(i), 0) + 1);
+
+        for(int i = 0; i < actual.length(); i++)
+            if(actual.charAt(i) != guess.charAt(i) && map.getOrDefault(guess.charAt(i), 0) > 0) {
+                map.put(guess.charAt(i), map.get(guess.charAt(i)) - 1);
+                score.pseudoHit++;
+            }
+        return score;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getScore("RGBY", "GGRR"));
+    }
+}
---
+++ src/ex615/Score.java
@@ -0,0 +1,14 @@
+package ex615;
+
+public class Score {
+    public int hit;
+    public int pseudoHit;
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "hit=" + hit +
+                ", pseudoHit=" + pseudoHit +
+                '}';
+    }
+}
