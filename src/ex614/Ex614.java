b099db223e439b398c90db410873456ac2554c3c
Tomasz Lichon <xorver@gmail.com>
2016-07-30 19:33:00 +0200

    Add faster implementation of Ex614.

--- src/ex614/Ex614.java
+++ src/ex614/Ex614.java
@@ -1,16 +1,14 @@
 package ex614;
 
 
+import utils.Line;
 import utils.Point;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Ex614 {
 
-    public static Collection<Point> largest_line(List<Point> points) {
+    public static Collection<Point> largestLine(List<Point> points) {
         List<Point> currentPoints;
         List<Point> bestPoints = new ArrayList<>();
 
@@ -19,8 +17,8 @@ public class Ex614 {
                 currentPoints = new ArrayList<>();
                 currentPoints.add(points.get(i));
                 currentPoints.add(points.get(j));
-                for (int k = j+1; k< points.size(); k++)
-                    if(VectorOperations.vector_product(points.get(i), points.get(j), points.get(k)) == 0){
+                for (int k = j+1; k < points.size(); k++)
+                    if(VectorOperations.vectorProduct(points.get(i), points.get(j), points.get(k)) == 0){
                         currentPoints.add(points.get(k));
                         if(currentPoints.size() > bestPoints.size())
                             bestPoints = currentPoints;
@@ -28,6 +26,24 @@ public class Ex614 {
             }
         return bestPoints;
     }
+
+    public static Line largestLineFast(List<Point> points) {
+        Map<Line, Integer> lines = new HashMap<>();
+        for (int i = 0; i < points.size(); i++)
+            for (int j = i + 1; j < points.size(); j++) {
+                Line line = new Line(points.get(i), points.get(j));
+                lines.put(line, lines.getOrDefault(line, 0) + 1);
+            }
+        Line bestLine = null;
+        int bestQuantity = 0;
+        for (Map.Entry<Line, Integer> entry: lines.entrySet())
+            if(entry.getValue() > bestQuantity) {
+                bestLine = entry.getKey();
+                bestQuantity = entry.getValue();
+            }
+        return bestLine;
+    }
+
 
     public static void main(String[] args) {
         List<Point> points = Arrays.asList(
@@ -42,6 +58,7 @@ public class Ex614 {
                 new Point(1, 7)
         );
 
-        System.out.println(largest_line(points));
+        System.out.println(largestLine(points));
+        System.out.println(largestLineFast(points));
     }
 }
--- src/utils/Line.java
+++ src/utils/Line.java
@@ -1,14 +1,20 @@
 package utils;
 
-import utils.Point;
+import ex614.VectorOperations;
 
 public class Line {
     public Point p1;
     public Point p2;
+    public double a;
+    public double b;
+    public double c;
 
     public Line(Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
+        this.a = - (p1.y - p2.y);
+        this.b = p1.x - p2.x;
+        this.c = - p1.y * (p1.x - p2.x) + p1.x * (p1.y - p2.y);
     }
 
     public String toEquationFormatString() {
@@ -23,5 +29,28 @@ public class Line {
                 "p1=" + p1 +
                 ", p2=" + p2 +
                 '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return (int)(this.a + this.b + this.c);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Line))
+            return false;
+        Line other = (Line) obj;
+        if(!VectorOperations.isZero(other.a) && !VectorOperations.isZero(this.a)
+                && !VectorOperations.isZero(other.b) && !VectorOperations.isZero(this.b))
+            return VectorOperations.isZero(other.b / other.a - this.b / this.a) &&
+                    VectorOperations.isZero(other.c / other.a - this.c / this.a);
+
+        if(!VectorOperations.isZero(other.a) && !VectorOperations.isZero(this.a))
+            return VectorOperations.isZero(other.c / other.a - this.c / this.a);
+
+        if(!VectorOperations.isZero(other.b) && !VectorOperations.isZero(this.b))
+            return VectorOperations.isZero(other.c / other.b - this.c / this.b);
+        return false;
     }
 }
