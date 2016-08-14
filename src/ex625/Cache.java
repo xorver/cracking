69376313537b63a4c39301f6d75c4da86759a1d4
Tomasz Lichon <t.j.lichon@gmail.com>
2016-08-14 13:26:39 +0200

    Add ex625 solution.

---
+++ src/ex625/Cache.java
@@ -0,0 +1,90 @@
+package ex625;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cache {
+
+    private int capacity;
+    private Map<Integer, Value> map = new HashMap<>();
+    private Map<Integer, Integer> seqMap = new HashMap<>();
+    int seq;
+    int lowestSeq;
+
+    public Cache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String get(int key) {
+        Value value = map.get(key);
+        if(value != null) {
+            seq++;
+            updateSeq(key, value, seq);
+            return value.string;
+        }
+        return null;
+    }
+
+    public String put(int key, String string) {
+        if(map.containsKey(key)) {
+            Value value = map.get(key);
+            seq++;
+            updateSeq(key, value, seq);
+            String oldString = value.string;
+            value.string = string;
+            normalize();
+            return oldString;
+        } else {
+            seq++;
+            Value value = new Value(string, seq);
+            map.put(key, value);
+            seqMap.put(seq, key);
+            normalize();
+            return null;
+        }
+    }
+
+    private void normalize() {
+        for(;lowestSeq<seq && map.size() > capacity; lowestSeq++) {
+            Integer key = seqMap.get(lowestSeq);
+            if(key != null) {
+                map.remove(key);
+                seqMap.remove(lowestSeq);
+            }
+        }
+    }
+
+    private void updateSeq(Integer key, Value value, int newSeq) {
+        seqMap.remove(value.seq);
+        value.seq = newSeq;
+        seqMap.put(newSeq, key);
+    }
+
+    @Override
+    public String toString() {
+        return "Cache{" +
+                "map=" + map +
+                '}';
+    }
+
+    private class Value
+    {
+        public String string;
+        public int seq;
+
+        @Override
+        public String toString() {
+            return "Value{" +
+                    "string='" + string + '\'' +
+                    ", seq=" + seq +
+                    '}';
+        }
+
+        public Value(String string, int seq) {
+            this.string = string;
+            this.seq = seq;
+
+        }
+    }
+
+}
---
+++ src/ex625/CacheLinkedList.java
@@ -0,0 +1,18 @@
+package ex625;
+
+public class CacheLinkedList {
+
+    private int capacity;
+
+    public CacheLinkedList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String get(int key) {
+        return null;
+    }
+
+    public String put(int key, String string) {
+        return null;
+    }
+}
